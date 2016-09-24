7e071e5e25dc5e664cfc383b7475c28f81743a61
MadinaIbrahim <madina.ibrahim.us@gmail.com>
2016-09-24 12:28:01 -0400

    Done

--- swift-fibo-finder-lab/ViewController.swift
+++ swift-fibo-finder-lab/ViewController.swift
@@ -12,14 +12,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        arrayWithFibonacciSequenceToIndex(5)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func arrayWithFibonacciSequenceToIndex(index: Int) -> [Int]{
+        
+        var fiboArray: [Int] = []
+        var a = 0
+        var b = 1
+        
+        if index == 0 {
+            fiboArray.append(0)
+        } else if index >= 1 {
+        for _ in 0 ..< index {
+            (a, b) = (b, a + b)
+            fiboArray.append(a)
+        }
+        print("Array with Fibonacci sequence to index \(index) will be \(fiboArray)")
+        } else {
+            print("The index has negative value.")
+        }
+        return fiboArray
     }
-
 
 }
 
